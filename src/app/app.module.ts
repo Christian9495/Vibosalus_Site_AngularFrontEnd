e7819fef0da4403f9567b104a37964755fc383de
Christian <christianm9495@gmail.com>
2020-06-16 12:37:41 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ServicesCarouselComponent } from './services-carousel/services-carousel.component';
+import { NewsComponent } from './news/news.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ServicesCarouselComponent,
+    NewsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
