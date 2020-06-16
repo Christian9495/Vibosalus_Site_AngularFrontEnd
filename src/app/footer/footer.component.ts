a33578b567d6029807986d09624a417bfd65587b
Christian <christianm9495@gmail.com>
2020-06-16 14:53:13 +0200

    graphics navbar update and insertion of the first graphical log-in function

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  logRequest: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  userLog(){
+    return this.logRequest;
+  }
+
+  isLogRequest(){
+    this.logRequest = !this.logRequest;
   }
 
 }
